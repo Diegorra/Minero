a5b76fe2a2c976a9c8fe6354edddef2b889192cb
Diego <Diegoramonsanchis@yahoo.es>
2020-04-28 13:18:00 +0200

    ...

---
+++ main/puntuaciones.ccp
@@ -0,0 +1,10 @@
+//Pr�ctica del minero FP2 versi�n 1
+//Realizada por Diego Ram�n Sanchis grupo A; FP2-A56 dieramon@ucm.es
+//				Diego Alvarez Carretero grupo A; FP2-A04
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <string.h>
+using namespace std;
+#include <ObjectArray.h>
+#include "puntuaciones.h"
---
+++ main/puntuaciones.h
@@ -0,0 +1,37 @@
+//Pr�ctica del minero FP2 versi�n 1
+//Realizada por Diego Ram�n Sanchis grupo A; FP2-A56 dieramon@ucm.es
+//				Diego Alvarez Carretero grupo A; FP2-A04
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <conio.h>
+using namespace std;
+#include <ObjectArray.h>
+
+const int TOTAL_MINAS = 5;
+typedef struct{
+    int Idmina, numMov, numGem, numTNT, puntos;
+}tDatosMina; //informacion de una mina 
+
+typedef struct{
+    string nombre;
+    int pun_total, numMinas;
+    tDatosMina vMinasRecorridas[TOTAL_MINAS];
+}tPuntuacionesJug; //informacion de un jugador
+
+typedef struct{
+    int capacidad, num_jugs;
+    tPuntuacionesJug *array_clasification;//array dinamico
+}tPuntuaciones; //informacion de todos los jugadores
+
+bool cargar_Marcador(tPuntuaciones& marcador);
+bool guardar_Marcador(tPuntuaciones& marcador);
+void mostar_Minas_usuario(const tPuntuaciones& marcador, int cont);
+void mostrar_Alfabetico(const tPuntuaciones& marcador);
+void mostar_Datos_Usuario(const tPuntuaciones& marcador);
+//funciones array dinamico
+void inicializa_Marcador(tPuntuaciones& marcador);
+void aumentar_capacidad(tPuntuaciones& marcador);
+void destruir(tPuntuaciones& marcador);
+bool buscar(tPuntuaciones& marcador, const string& nombre, int& pos);
+void insertar(tPuntuaciones& marcador, string const& nombre, int pos);
